88f7826e97227c3f3a44c16fbd7d0a71754d3f06
Andrzej <andymatuszczyk@wp.pl>
2025-11-23 13:49:38 +0100

    sql_alchemy1

---
+++ models.py
@@ -0,0 +1,50 @@
+from sqlalchemy import (
+    Column, Integer, String, Float, ForeignKey
+)
+from sqlalchemy.orm import declarative_base, relationship
+
+Base = declarative_base()
+
+
+class Movie(Base):
+    __tablename__ = "movies"
+
+    movieId = Column(Integer, primary_key=True)
+    title = Column(String, nullable=False)
+    genres = Column(String, nullable=False)
+
+    ratings = relationship("Rating", back_populates="movie")
+    tags = relationship("Tag", back_populates="movie")
+    links = relationship("Link", back_populates="movie", uselist=False)
+
+
+class Rating(Base):
+    __tablename__ = "ratings"
+
+    userId = Column(Integer, primary_key=True)
+    movieId = Column(Integer, ForeignKey("movies.movieId"), primary_key=True)
+    rating = Column(Float, nullable=False)
+    timestamp = Column(Integer)
+
+    movie = relationship("Movie", back_populates="ratings")
+
+
+class Tag(Base):
+    __tablename__ = "tags"
+
+    userId = Column(Integer, primary_key=True)
+    movieId = Column(Integer, ForeignKey("movies.movieId"), primary_key=True)
+    tag = Column(String)
+    timestamp = Column(Integer)
+
+    movie = relationship("Movie", back_populates="tags")
+
+
+class Link(Base):
+    __tablename__ = "links"
+
+    movieId = Column(Integer, ForeignKey("movies.movieId"), primary_key=True)
+    imdbId = Column(String)
+    tmdbId = Column(String)
+
+    movie = relationship("Movie", back_populates="links")
